b16e14bf6a0133761fb050c7f137de9e1fb9f3f2
arhip <arhip.arhip2002@yandex.ru>
2019-01-22 10:33:23 +0300

    update

--- test_bind.py
+++ test_bind.py
@@ -13,7 +13,9 @@ from time import sleep
 class MainWidget(FloatLayout):
     def __init__(self):
         super(MainWidget, self).__init__()
-        self.map = GridLayout(cols=16,rows=12,pos_hint={'center_x':0.5,'center_y':0.5},size_hint = (1,1))
+        self.player_y = 0.5
+        self.player_x = 0.5
+        self.map = GridLayout(cols=23,rows=12,pos_hint = {'center_y':self.player_y, 'center_x':self.player_x},size_hint = (23/16,1))
         self.add_widget(self.map)
         f = open('polygon.txt')
         for line in f:
@@ -22,37 +24,37 @@ class MainWidget(FloatLayout):
                     self.map.add_widget(Image(source='sprites/wall1.png',allow_stretch=True))
                 elif p == '.':
                     self.map.add_widget(Image(source='sprites/floor3.png',allow_stretch=True))
+        self.add_widget(Image(source='sprites/floor_1.png',pos_hint={'center_x':0,'center_y':0}))
 
 
 
-        self.player_y = 0.5
-        self.player_x = 0.5
-        self.player = Image(size_hint=(1/16,1/12),pos_hint = {'center_y':self.player_y, 'center_x':self.player_x},source='sprites/player.gif',allow_stretch=True,anim_delay=0.25)
+
+        self.player = Image(size_hint=(1/16,1/12),pos_hint={'center_x':0.5,'center_y':0.5},source='sprites/player.gif',allow_stretch=True,anim_delay=0.25)
         self.add_widget(self.player)
 
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
     def update_pos(self):
-        self.player.pos_hint = {'center_y':self.player_y, 'center_x':self.player_x}
+        self.map.pos_hint = {'center_y':self.player_y, 'center_x':self.player_x}
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        if keycode[1] == 'w' or keycode[1] == 'up':
+        if keycode[1] == 's' or keycode[1] == 'down':
             self.player_y += 1/24
             self.player_y = min(21/24,self.player_y)
             self.update_pos()
-        elif keycode[1] == 's' or keycode[1] == 'down':
+        elif keycode[1] == 'w' or keycode[1] == 'up':
             self.player_y -= 1/24
             self.player_y = max(3/24,self.player_y)
             self.update_pos()
-        elif keycode[1] == 'a' or keycode[1] == 'left':
-            self.player_x -=1/32
-            self.player_x = max(3/32,self.player_x)
-            self.update_pos()
         elif keycode[1] == 'd' or keycode[1] == 'right':
+            self.player_x -=1/32
+            self.player_x = max(-4/32,self.player_x)
+            self.update_pos()
+        elif keycode[1] == 'a' or keycode[1] == 'left':
             self.player_x +=1/32
-            self.player_x = min(29/32,self.player_x)
+            self.player_x = min(36/32,self.player_x)
             self.update_pos()
         return True
 
@@ -61,3 +63,8 @@ class MainApp(App):
     def build(self):
         return MainWidget()
 MainApp().run()
+
+
+class Player(object):
+    def __init__(self,atack_range=64):
+        pass
